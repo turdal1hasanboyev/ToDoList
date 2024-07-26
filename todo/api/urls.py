d0f7be60aacb50a07bc9a5c4616cb4f8edffdbf3
turdal1hasanboyev <turdalihasanboyev70.com>
2024-07-26 10:47:13 +0500

    Writing Todo api!

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -9,7 +9,7 @@ app_name = 'todo'
 urlpatterns = [
     path('todolist/', ToDoListView.as_view(), name='todo-list'),
     path('tododetail/<int:pk>/', ToDoDetaliView.as_view(), name='todo-detail'),
-    path('todoupdate/<int:pk>/update/', ToDoUpdateView.as_view(), name='todo-update'),
+    path('todoupdate/<int:pk>/', ToDoUpdateView.as_view(), name='todo-update'),
     path('todocreate/', ToDoCreateView.as_view(), name='todo-create'),
     path('tododelete/<int:pk>/', ToDoDestroyView.as_view(), name='todo-delete'),
 ]
