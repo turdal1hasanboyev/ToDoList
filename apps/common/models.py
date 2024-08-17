9f6ec6d0ee08ca1a1893035de7afc894e06e8eb7
turdal1hasanboyev <turdalihasanboyev70.com>
2024-08-17 08:00:58 +0500

    Updating project and Finish Project!

--- apps/common/models.py
+++ apps/common/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    is_done = models.BooleanField(default=False)
+    is_done = models.BooleanField(default=False, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
